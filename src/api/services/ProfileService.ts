106a9c11aabd28f61f41a6e40881f5f00ffd4f5f
waseeen <aaaa@waseeen.ru>
2025-02-27 12:00:27 +0300

    fix: no requests without token

--- src/api/services/ProfileService.ts
+++ src/api/services/ProfileService.ts
@@ -2,8 +2,12 @@ import httpService from './HttpService';
 import { ProfileData } from '../../types/ProfileData';
 
 export const getProfile = async (): Promise<ProfileData | null> => {
+  if (!localStorage.getItem('token')) return null;
   const profile = await httpService.get('/profile');
-  if (!profile) return null;
+  if (!profile) {
+    localStorage.removeItem('token');
+    return null;
+  }
   return profile.data as ProfileData;
 };
 
